191e3f6fd0872a13a9408dfaf55ee529e0a0c0aa
JrzenonDev <jrobertoonb@gmail.com>
2022-05-30 20:05:06 -0300

    create function handleLogin

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../../contexts/Auth/AuthContext"
 
 export const Login = () => {
 
-  const [email] = useState('')
-  const [password] = useState('')
+  const auth = useContext(AuthContext)
+  const navigate = useNavigate()
 
-  const handleLogin = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
+  const handleLogin = async () => {
+    if (email && password) {
+      const isLogged = await auth.signin(email, password)
+      if (isLogged) {
+        navigate('/')
+      } else {
+        alert('Erro ao se logar')
+      }
+    }
   }
 
   return(
@@ -17,11 +29,13 @@ export const Login = () => {
         type="text"
         value={email}
         placeholder="Digite o seu e-mail"
+        onChange={e => setEmail(e.target.value)}
       />
       <input
         type="text"
         value={password}
         placeholder="Digite a sua senha"
+        onChange={e => setPassword(e.target.value)}
       />
       <button onClick={handleLogin}>Logar</button>
     </div>
